09a844c999b1f5a267ec55acb4d383365d7a20d9
Kareem James <kareem.james@detroitlabs.com>
2019-07-09 00:59:47 -0400

    initial commit

---
+++ src/main/java/com/hoodwink/userqueue/UserqueueApplication.java
@@ -0,0 +1,13 @@
+package com.hoodwink.userqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserqueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserqueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoodwink/userqueue/UserqueueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hoodwink.userqueue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserqueueApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
