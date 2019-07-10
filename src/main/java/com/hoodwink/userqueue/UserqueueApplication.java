4aa689040c16fc92da7bdca4aa0cb0a69a696f91
Kareem James <kareem.james@detroitlabs.com>
2019-07-09 23:08:21 -0400

    add jasypt encryption to encrypt db password in application properties, successfully encrypted password for db and able to connect

--- src/main/java/com/hoodwink/userqueue/UserqueueApplication.java
+++ src/main/java/com/hoodwink/userqueue/UserqueueApplication.java
@@ -1,5 +1,6 @@
 package com.hoodwink.userqueue;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
